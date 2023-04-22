dd6640d2cc9781d7f6642cd681e549c33761be4e
Rachalle Way <way.rachalle@gmail.com>
2023-04-22 09:41:28 -0400

    solved

--- index.js
+++ index.js
@@ -9,7 +9,31 @@ const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnVa
 const makeTag = tag => str => `<${tag}>${str}</${tag}>`
 
 // complete this function
-const makePoemHTML = () => {}
+const makePoemHTML = (data) => {
+  var poem = ''
+
+  const [{title, author, lines}] = data
+
+  //title and author should be h2 and h3
+  poem +=
+    makeTag(`h2`)(title) + pipe(makeTag(`em`), makeTag(`h3`))(`by ` + author)
+
+  //create stanzas
+  var joinLines = arr => arr.join(`<br>`)
+  var splitLines = str => str.split(`<br><br>`)
+
+  var makeStanzas = pipe(joinLines, splitLines)
+  var makePoem = makeTag(`p`)
+
+  var paraArray = makeStanzas(lines).map(str => makePoem(str))
+
+  var paraString = paraArray.join(``)
+
+  poem +=
+    paraString
+  
+  return poem
+}
 
 // attach a click event to #get-poem
 getPoemBtn.onclick = async function() {
